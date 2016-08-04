652b374b3755c67e698ead9c92608d1c94a0575c
Colin Olito <colin.olito@gmail.com>
2016-08-05 09:59:26 +1000

    skeleton functions for gamete haplotype frequencies in functions-Simulations.R file

--- R/functions-Simulations.R
+++ R/functions-Simulations.R
@@ -21,10 +21,10 @@
 #########################################
 ## Average fitness through each sex role
 
-Wf.av  <-  function(Fii.list, Wf.list, par.list, ...){
+Wf.av  <-  function(Fii.list, Wf.list, ...){
    (F11*wf11) + (F12*wf12) + (F13*wf13) + (F14*wf14) + (F22*wf22) + (F23*wf23) + (F24*wf24) + (F33*wf33) + (F34*wf34) + (F44*wf44)
 }
-Wm.av  <-  function(Fii.list, Wm.list, par.list, ...){
+Wm.av  <-  function(Fii.list, Wm.list, ...){
    (F11*wm11) + (F12*wm12) + (F13*wm13) + (F14*wm14) + (F22*wm22) + (F23*wm23) + (F24*wm24) + (F33*wm33) + (F34*wm34) + (F44*wm44)
 }
 
@@ -32,16 +32,44 @@ Wm.av  <-  function(Fii.list, Wm.list, par.list, ...){
 #########################################
 ## Haplotype frequency change in gametes
 
-
-x1p  <-  function(Fii, Wf, Wm, ...) {
-
+#  Ovules
+x1p  <-  function(Fii.list, Wf.list, Wm.list, par.list,...) {
+	((2*F11*wf11) + (F12*wf12) + (F13*wf13) + (F14*wf14)) / (2*Wf.av(Fii.list, Wf.list)) - 
+		rf*(((F14*wf14) - (F23*wf23))/(2*Wf.av(Fii.list, Wf.list)))
 }
 
+x2p  <-  function(Fii.list, Wf.list, Wm.list, par.list,...){
+	((2*F22*wf22) + (F12*wf12) + (F23*wf23) + (F24*wf24))/(2*Wf.av(Fii.list, Wf.list)) + 
+		rf*(((F14*wf14) - (F23*wf23))/(2*Wf.av()))
+}
 
-x1p[wf11_, wf12_, wf13_, wf14_, wf22_, wf23_, wf24_, wf33_, wf34_, wf44_][rf_][C_] := 
+x3p  <-  function(Fii.list, Wf.list, Wm.list, par.list,...) {
+	 ((2*F33*wf33) + (F34*wf34) + (F13*wf13) + (F23*wf23))/(2*Wf.av()) + 
+ 		rf*(((F14*wf14) - (F23*wf23))/(2*Wfav))
+}
 
-((2*\[Phi]11[q1, C]*wf11) + (\[Phi]12[q1, q2, C]*
-      wf12) + (\[Phi]13[q1, q3, C]*wf13) + (\[Phi]14[q1, q4, C]*
-      wf14)) / (2*Wfav[q1, q2, q3, q4][wf11, wf12, wf13, wf14, wf22, wf23, wf24, wf33, wf34, wf44][C]) - rf*(((\[Phi]14[q1, q4, C]*wf14) - (\[Phi]23[q2, q3, C]*wf23))/(
-     2*Wfav[q1, q2, q3, q4][wf11, wf12, wf13, wf14, wf22, wf23, wf24, 
-        wf33, wf34, wf44][C]));
+x4p  <-  function(Fii.list, Wf.list, Wm.list, par.list,...) {
+	((2*F44*wf44) + (F34*wf34) + (F14*wf14) + (F24*wf24))/(2*Wf.av()) - 
+		rf*(((F14*wf14) - (F23*wf23))/(2*Wf.av()))
+}
+
+#  Pollen/Sperm
+y1p  <-  function(Fii.list, Wf.list, Wm.list, par.list,...) {
+	((2*F11*wm11) + (F12*wm12) + (F13*wm13) + (F14*wm14)) / (2*Wm.av(Fii.list, Wm.list)) - 
+		rf*(((F14*wm14) - (F23*wm23))/(2*Wm.av(Fii.list, Wm.list)))
+}
+
+y2p  <-  function(Fii.list, Wm.list, Wm.list, par.list,...){
+	((2*F22*wm22) + (F12*wm12) + (F23*wm23) + (F24*wm24))/(2*Wm.av(Fii.list, Wm.list)) + 
+		rf*(((F14*wm14) - (F23*wm23))/(2*Wm.av()))
+}
+
+y3p  <-  function(Fii.list, Wm.list, Wm.list, par.list,...) {
+	 ((2*F33*wm33) + (F34*wm34) + (F13*wm13) + (F23*wm23))/(2*Wm.av()) + 
+ 		rf*(((F14*wm14) - (F23*wm23))/(2*Wmav))
+}
+
+y4p  <-  function(Fii.list, Wm.list, Wm.list, par.list,...) {
+	((2*F44*wm44) + (F34*wm34) + (F14*wm14) + (F24*wm24))/(2*Wm.av()) - 
+		rf*(((F14*wm14) - (F23*wm23))/(2*Wm.av()))
+}
